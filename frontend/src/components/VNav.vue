85c12ea335f7774cfb4472cf2c134b4bf30587eb
cwj <wenjingcui900@gmail.com>
2023-12-11 13:51:51 +0800

    修改菜单栏和登陆界面

--- frontend/src/components/VNav.vue
+++ frontend/src/components/VNav.vue
@@ -1,65 +1,42 @@
 <!--
- * @Description:
- * @Author: l
- * @Date: 2021-06-01 14:39:02
- * @LastEditors: l
- * @LastEditTime: 2021-06-27 00:15:02
  * @FilePath: \DistributedControlSystem\frontend\src\components\VNav.vue
 -->
 <template>
-  <!-- <nav class="vnav">
-    <div class="container">
-      <a class="nav-brand">Distributed Control System</a>
-      <div class='nav-collapse'>
-      <ul class="menu">
-        <li><router-link to="/home">home</router-link></li>
-        <li><router-link to="/login">login</router-link></li>
-        <li><router-link to="/register">register</router-link></li>
-        <li><router-link to="/room">room</router-link></li>
-        <li><router-link to="/administrator">administrator</router-link></li>
-        <li><router-link to="/manager">manager</router-link></li>
-        <li><router-link to="/receptionist">receptionist</router-link></li>
-      </ul>
-      </div>
-    </div>
-  </nav> -->
-
-  <el-menu
-    :default-active="$route.path"
-    class="my-menu"
-    mode="horizontal"
-    router
-    style=" backdrop-filter: blur(15px);
-    background: linear-gradient(rgba(255, 255, 255, 0.1), rgba(0, 0, 0, 0.5));
-    box-shadow: 0 20px 40px 1px rgba(0, 0, 0, 0.12);"
-    text-color="#fff"
-    active-text-color="#ffd04b"
-  >
+  <el-menu :default-active="$route.path" class="menu" mode="horizontal" router text-color="white" >
     <el-menu-item index="/login" route="/login">登录</el-menu-item>
-    <!-- <el-menu-item index="3" route="/register">注册</el-menu-item> -->
-    <el-menu-item index="/room" route="/room" :disabled="false"
-      >用户</el-menu-item
-    >
-    <el-menu-item
-      index="/administrator"
-      route="/administrator"
-      :disabled="false"
-      >管理员</el-menu-item
-    >
-    <el-menu-item index="/receptionist" route="/receptionist" :disabled="false"
-      >前台</el-menu-item
-    >
-    <el-menu-item index="/manager" route="/manager" :disabled="false"
-      >经理</el-menu-item
-    >
+    <el-menu-item index="/home" route="/home">主页面</el-menu-item>
+    <el-menu-item index="/room" route="/room" :disabled="false">用户</el-menu-item>
+    <el-menu-item index="/administrator" route="/administrator" :disabled="false">管理员</el-menu-item>
+    <el-menu-item index="/receptionist" route="/receptionist" :disabled="false">前台</el-menu-item>
+    <el-menu-item index="/manager" route="/manager" :disabled="false">经理</el-menu-item>
   </el-menu>
 </template>
 
 <script>
 import { defineComponent } from "@vue/composition-api";
-import router from "../router";
+import router from '../router';
 export default defineComponent({
-  setup() {}
+  
+  setup() {},
 });
 </script>
-<style scoped></style>
+<style scoped>
+  .menu {
+    background-color: #486589; 
+  }
+
+  .menu .el-menu-item {
+    font-size: 16px;
+  }
+
+  .menu .el-menu-item:not(.is-disabled):hover {
+    color: #0b1525;
+  }
+
+  .menu .el-menu-item.is-active {
+    font-weight: bold; 
+    color: #182f54;
+    background-color: white;
+  }
+
+</style>
--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,5 +1,5 @@
 <!--
- * @Description:
+ * @Description: 
  * @Author: l
  * @Date: 2021-06-01 10:29:38
  * @LastEditors: l
@@ -8,79 +8,54 @@
 -->
 <template>
   <div>
-    <!-- <form action="" method="post" claspxs="smart-green">
-
-      <h1>Login</h1>
-      <label>
-        <span>Your User Name :</span>
-        <input
-          id="username"
-          type="text"
-          name="username"
-          placeholder="Your User Name"
-          v-model="userName"
-        />
-      </label>
-      <label>
-        <span>Your Password :</span>
-        <input
-          id="password"
-          type="text"
-          name="password"
-          placeholder="Your Password"
-          v-model="password"
-        />
-      </label>
-      <label>
-        <span>&nbsp;</span>
-        <input
-          type="button"
-          class="button"
-          value="Login"
-          @click="loginAdmin({ userName, password })"
-        />
-      </label>
-    </form> -->
-    <div class="content">
-      <el-form label-width="60px" size="small" class="login-form">
-        <!-- <el-form-item label="用户名">
-        <el-input v-model="userName"></el-input>
-      </el-form-item> -->
-        <el-row align="middle" type="flex">
-          <el-col :span="10"><h1>账号 ：</h1></el-col>
-          <el-col :span="10">
-            <el-autocomplete
-              class="inline-input"
-              v-model="userName"
-              :fetch-suggestions="querySearch"
-              placeholder="请输入账号"
-            ></el-autocomplete>
-          </el-col>
-        </el-row>
-        <el-row align="middle" type="flex" style="margin-top: 20px">
-          <el-col :span="10"><h1>密码:</h1></el-col>
-          <el-col :span="10">
-            <el-input
-              v-model="password"
-              show-password
-              placeholder="请输入密码"
-            ></el-input>
-          </el-col>
-        </el-row>
-        <el-row align="middle" type="flex" style="margin-top: 20px">
-          <el-col :span="8" :offset="3">
-            <el-button
-              type="primary"
-              @click="loginAdmin({ userName, password })"
-            >
-              <h1>登录</h1>
-            </el-button>
-          </el-col>
-          <el-col :span="10">
-            <el-button @click="reset"><h1>重置</h1></el-button>
-          </el-col>
-        </el-row>
-      </el-form>
+    <div class="background">
+    </div> 
+    <div class="header">
+      <div class="left-section">
+        <h1 >波普特廉价酒店|空调管理系统</h1>
+      </div>
+      <div class="right-section"></div>
+    </div>
+    <div class="container">
+      <div class="left-image">
+        <img :src="imgSrc" height="70%" alt="left_image" />
+      </div> 
+      <div class="right-form">
+        <el-form label-width="70px" size="small" class="login-form">
+          <!-- <el-form-item label="用户名">
+            <el-input v-model="userName"></el-input>
+          </el-form-item> -->
+          <h3>账号登录</h3>
+          <el-row align="middle" type="margin-top: 20px">
+            <el-col :span="10"> 账号: </el-col>
+            <el-col :span="10">
+              <el-autocomplete class="inline-input" v-model="userName":fetch-suggestions="querySearch" placeholder="请输入您的账号"
+              ></el-autocomplete>
+            </el-col>
+          </el-row>
+          <el-row align="middle" type="flex" style="margin-top: 20px">
+            <el-col :span="10"> 密码: </el-col>
+            <el-col :span="10">
+              <el-input
+                v-model="password"
+                show-password
+                placeholder="请输入密码"
+              ></el-input>
+            </el-col>
+          </el-row>
+          <el-row align="middle" type="flex" style="margin-top: 20px">
+            <el-col :span="8" :offset="3">
+              <el-button type="primary" @click="loginAdmin({ userName, password })">
+                登录
+              </el-button>
+            </el-col>
+            <el-col :span="10">
+              <el-button @click="reset">重置</el-button>
+            </el-col>
+          </el-row>
+        </el-form>
+      </div>
+      
     </div>
   </div>
 </template>
@@ -92,7 +67,7 @@ import { defineComponent } from "@vue/composition-api";
 export default defineComponent({
   setup() {},
   name: "Login",
-  data: function() {
+  data: function () {
     return {
       userList: [
         { value: "room_1" },
@@ -101,41 +76,19 @@ export default defineComponent({
         { value: "room_4" },
         { value: "receptionist_1" },
         { value: "manager_1" },
-        { value: "administrator_1" }
+        { value: "administrator_1" },
       ],
       userName: "",
       password: "",
-      imgSrc: require("../assets/images/room.jpg")
+      imgSrc: require("../assets/images/room.jpg"),
     };
   },
   computed: {
-    ...mapState("auth", ["role"])
-  },
-  watch: {
-    // role: function () {
-    //   console.log("current role:", this.role);
-    //   switch (this.role) {
-    //     case "room":
-    //       this.$router.replace("/room");
-    //       break;
-    //     case "administrator":
-    //       this.$router.replace("/administrator");
-    //       break;
-    //     case "manager":
-    //       this.$router.replace("/manager");
-    //       break;
-    //     case "receptionist":
-    //       this.$router.replace("/receptionist");
-    //       break;
-    //     default:
-    //       console.log("illegal role");
-    //       this.$router.replace("/home");
-    //       break;
-    //   }
+    ...mapState("auth", ["role"]),
   },
   methods: {
     ...mapActions("auth", ["loginAdmin"]),
-    reset: function() {
+    reset: function () {
       console.log("reset");
       this.userName = "";
       this.password = "";
@@ -147,30 +100,78 @@ export default defineComponent({
         : userList;
       // 调用 callback 返回建议列表的数据
       cb(results);
-    }
-  }
+    },
+  },
 });
 </script>
 <style scoped>
 .background {
-  top: 0;
-  margin-top: 0px;
+  background-color: rgb(194, 220, 220);
+  top :0px; ;
   width: 100%;
   height: 100%; /**宽高100%是为了图片铺满屏幕 */
   z-index: -1;
   position: absolute;
 }
 
+.header{
+  margin-top: 2.5%;
+  display: flex;
+}
+
+.left-section{
+  flex: 3;
+  display: flex;
+  justify-content: left;
+  align-items:flex-start;
+  margin-left: 10%;
+  margin-bottom: 1%;
+}
+.right-section{
+  flex: 2;
+  display: flex;
+  justify-content: left;
+  align-items: flex-start;
+}
+
+.container {
+  display: flex;
+  height: 100vh;
+  align-items: stretch;
+}
+
+.left-image{
+  flex: 3;
+  display: flex;
+  justify-content: right;
+  align-items:flex-start;
+  margin-bottom: 10%;
+
+}
+
+.left-image image{
+  width: 25%; /* 图片的最大宽度为容器宽度 */
+  height: auto; /* 高度自动调整 */
+  object-fit: contain; /* 保持图片内容在框架内且不失真 */
+  margin-left: 5%;
+}
+
+.right-form{
+  flex: 2;
+  display: flex;
+  justify-content: left;
+  align-items: flex-start;
+  margin-bottom: 10%;
+  margin-left: 5%;
+}
+
 .login-form {
-  margin-right: 35%;
-  margin-left: 35%;
-  /* margin-top: 50px; */
-  border-radius: 30px;
+  /* position: absolute; */
+  right: 10%;
+  height: 70%;
+  width: 60%;
+  border-radius: 10px;
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
-  padding-top: 30px;
-  padding-left: 40px;
-  padding-right: 40px;
-  padding-bottom: 25px;
   background-color: rgba(255, 255, 255, 0.8);
 }
 </style>
